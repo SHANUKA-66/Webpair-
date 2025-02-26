b9020696356f257be38e8142d743eb4d6c00dadc
SHANUKA-66 <githubacc452@gmail.com>
2025-02-26 22:41:22 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'sajjadaslam923@gmail.com',
-    password: '002821##Silen',
+    email: 'Wilov27903@jarars.com ',
+    password: 'H4-M:ebN8t.CE!i',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
